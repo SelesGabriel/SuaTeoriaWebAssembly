0b0de6fb4a216f1334efc5e68141405c7d738de9
Gabriel da Silva Seles <selesgabriel@gmail.com>
2022-01-24 21:10:41 -0300

    Index, As mais faladas, teoristas, animes e mangas, series, filmes e hqs: Finalizados a casca.

---
+++ SuaTeoriaWebAssembly/Classes/Teoria.cs
@@ -0,0 +1,10 @@
+﻿namespace SuaTeoriaWebAssembly.Classes
+{
+    public class Teoria
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
