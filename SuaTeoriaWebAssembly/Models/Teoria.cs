4db36b9e838ebc25859712b045fab9df85f757e7
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2022-06-06 07:27:53 -0300

    update

--- SuaTeoriaWebAssembly/Models/Teoria.cs
+++ SuaTeoriaWebAssembly/Models/Teoria.cs
@@ -1,13 +1,20 @@
-﻿namespace SuaTeoriaWebAssembly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuaTeoriaWebAssembly.Models
 {
     public class Teoria
     {
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Conteudo { get; set; }
+        [Required]
         public string ImagemTitulo { get; set; }
         public DateTime DataCriacao { get; set;} = DateTime.Now;
+        [Required]
         public int IdCategoria { get; set; }
+        [Required]
         public int IdObra { get; set; }
         public int IdUsuario { get; set; }
         public string NovaObra { get; set; }
