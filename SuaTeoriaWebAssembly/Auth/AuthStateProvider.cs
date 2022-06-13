c6f3ef32481ef5bbbaa1eaacdbcce31310f5dbd3
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2022-06-13 20:21:50 -0300

    Inicio Authentication

---
+++ SuaTeoriaWebAssembly/Auth/AuthStateProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace SuaTeoriaWebAssembly.Auth
+{
+    public class AuthStateProvider:AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Gabriel Silva")
+            },"selesgabriel");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
--- SuaTeoriaWebAssembly/Program.cs
+++ SuaTeoriaWebAssembly/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SuaTeoriaWebAssembly;
+using SuaTeoriaWebAssembly.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+
+//builder.Services.AddAuthorizationCore()
+//builder.Services.addAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
