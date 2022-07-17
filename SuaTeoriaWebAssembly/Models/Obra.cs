abff5a7cccb090f3e6907bf5cb73f2e786fb127a
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2022-07-17 17:32:53 -0300

    Ajustes na visualização

--- SuaTeoriaWebAssembly/Models/Obra.cs
+++ SuaTeoriaWebAssembly/Models/Obra.cs
@@ -6,5 +6,6 @@
         public string Nome { get; set; }
         public int IdCategoria { get; set; }
         public string ImagemObra { get; set; }
+        public string Descricao{ get; set; }
     }
 }
