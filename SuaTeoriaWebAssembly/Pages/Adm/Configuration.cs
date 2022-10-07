49b1beaf65b5634e7ff621778a8db07d64469652
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2022-10-07 09:27:30 -0300

    Configurando base URL

---
+++ SuaTeoriaWebAssembly/Pages/Adm/Configuration.cs
@@ -0,0 +1,6 @@
+﻿public static class Configuration
+{
+    //public static string BaseUrl { get; set; } = "https://suateoriaapi.azurewebsites.net";
+    public static string BaseUrl { get; set; } = "http://localhost:5285";
+}
+
