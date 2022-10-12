2d2bf42544477566b1aee686595a5066709e759b
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2022-10-12 19:13:23 -0300

    update

---
+++ SuaTeoriaWebAssembly/Pages/Adm/FileUpload.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+
+namespace SuaTeoriaWebAssembly.Pages.Adm
+{
+    public static class FileUpload
+    {
+        public static string UploadBase64Image(string base64Image, string container)
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=suateoria;AccountKey=CuZyhXE1qt56tx1b5MlvzlLpklEDKohMZvIDPmmrR8ATMJJ59sr8iY8eGYI8OZNnlqmXEkPwf7yA+ASta7Y7/w==;EndpointSuffix=core.windows.net";
+            var fileName = Guid.NewGuid().ToString() + ".jpg";
+            byte[] imageBytes = Convert.FromBase64String(base64Image);
+            var blobClient = new BlobClient(connectionString, container, fileName);
+            using (var stream = new MemoryStream(imageBytes))
+            {
+                blobClient.Upload(stream);
+            }
+
+            return blobClient.Uri.AbsoluteUri;
+
+        }
+    }
+}
--- SuaTeoriaWebAssembly/wwwroot/js/JavaScript.js
+++ SuaTeoriaWebAssembly/wwwroot/js/JavaScript.js
@@ -10,3 +10,41 @@ function getHtml() {
     var htmlText = document.getElementById("textoTeoria").innerHTML;
     return htmlText;
 }
+
+
+
+function buscaImagem(conteudo) {
+    // criando um HTML com outros elementos, inclusive uma imagem que não tem src com Base 64
+    const string = `<img src="data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAakAAACYCAYAAABefRbnAAAgAElEQVR4AeydBXRcR7KG9+2+t2/hBTeJ4zh2bAccO2ZmZmZmZmYZZGZmkJmZmZmZSZJl5sSB3ay99c5Xo55cT2RbcgQzSuscnTtzp2/f7urqou7+6w/fBV+TkSuQmCC" alt="">
+<p>outra tag</p>
+<img src="nao_sou_base64.png">
+<img src="data:image/jpeg;base64,FqT8XomvHz8iASWKq5HEADIS7dqmlRQvWEC2rVj6GHjxW6uG9eW111+X2J/ElLffekv+d+PKE2VwdNtmefPNN2Xp9zOfeI2/dmCC" alt="">`;
+
+    // cria o template e coloca a string nele
+    const template = document.createElement('template');
+    template.innerHTML = conteudo;
+    // pega as imagens que tem src com Base 64 e troca para o valor default
+    const uri = "http://localhost:5285/uploadimage";
+    for (const img of template.content.querySelectorAll('img[src^="data:image/jpeg;base64,"]')) {
+        fetch(uri, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: img
+        })
+            .then(response => response.json())
+            .then(() => {
+                getItems();
+                addNameTextbox.value = '';
+            })
+            .catch(error => console.error('Unable to add item.', error));
+    }
+
+
+
+
+
+    return "";
+}
